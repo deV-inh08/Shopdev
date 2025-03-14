dc9ccfd6a2de27778b153851e682cbe664ac1166
deV_inh08 <vintd2004@gmail.com>
2025-03-14 18:14:24 +0700

    Save changes before rebase

--- Backend/src/services/user.services.ts
+++ Backend/src/services/user.services.ts
@@ -1,35 +1,29 @@
-import { ObjectId } from 'mongodb'
-import type { StringValue } from 'ms'
-import { TokenType } from '~/constants/enums'
-import { RegisterBody } from '~/models/request/user.request'
-import { signToken } from '~/utils/jwt'
+// import { ObjectId } from 'mongodb'
+// import type { StringValue } from 'ms'
+// import { TokenType } from '~/constants/enums'
+// import { RegisterBody } from '~/models/request/user.request'
+// import { signToken } from '~/utils/jwt'
 
-class UserServices {
-  // sign with: user_id, veerify status
-  private signEmailVerifyToken({ user_id, verify }: { user_id: string, verify: string }) {
-    return signToken({
-      payload: {
-        user_id,
-        token_type: TokenType.EmailVerifyToken,
-        verify
-      },
-      privateKey: process.env.JWT_SECRET_REFRESH_TOKEN as string,
-      option: {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN as StringValue
-      }
-    })
-  }
+// class UserServices {
+//   // sign with: user_id, veerify status
+//   private signEmailVerifyToken({ user_id, verify }: { user_id: string, verify: string }) {
+//     return signToken({
+//       payload: {
+//         user_id,
+//         token_type: TokenType.EmailVerifyToken,
+//         verify
+//       },
+//       privateKey: process.env.JWT_SECRET_REFRESH_TOKEN as string,
+//       option: {
+//         expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN as StringValue
+//       }
+//     })
+//   }
 
-  register(payload: RegisterBody) {
-    const user_id = new ObjectId()
-    // verify token (JWT) => sign_email_token
-<<<<<<< HEAD
-    // const email_verify_token =
-=======
-    // const email_verify_token = 
->>>>>>> 545267a (signemailverify)
-  }
-}
+//   register(payload: RegisterBody) {
+//     const user_id = new ObjectId()
+//   }
+// }
 
-const userServices = new UserServices()
-export default userServices
+// const userServices = new UserServices()
+// export default userServices
